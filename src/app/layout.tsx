ddd442ad31f93a6413fd858c3c39d8523c6090b9
aditya <adityakotkar47@gmail.com>
2025-01-24 18:37:21 +0530

    theme fix #4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,11 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           <Sidebar />
-          <main className="min-h-screen pl-64 pt-16 bg-gradient-to-br from-background via-background to-secondary-50/30 dark:from-background-dark dark:via-background-dark dark:to-theme-nature/5">
-            <div className="container mx-auto p-6">
-              {children}
+          <main className="min-h-screen pl-64 pt-16">
+            <div className="content-pattern min-h-[calc(100vh-4rem)]">
+              <div className="container mx-auto p-6">
+                {children}
+              </div>
             </div>
           </main>
         </Providers>
