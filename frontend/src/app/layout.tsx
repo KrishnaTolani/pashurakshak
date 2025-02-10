9d8c193143cc56b3d17caccc59de0816df642655
aditya <adityakotkar47@gmail.com>
2025-02-10 11:10:15 +0530

    fix: remove horizontal scrollbar from dashboard

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,13 +25,13 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} ${montserrat.variable}`}>
         <Providers>
-          <div className="flex min-h-screen flex-col">
+          <div className="flex min-h-screen flex-col overflow-x-hidden">
             <Navbar />
             <div className="flex flex-1">
               <Sidebar />
-              <main className="flex-1">
+              <main className="flex-1 w-0">
                 <div className="content-pattern min-h-full">
-                  <div className="container mx-auto p-6">
+                  <div className="container mx-auto p-6 max-w-[100vw]">
                     {children}
                   </div>
                 </div>
